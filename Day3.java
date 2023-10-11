9afa4c70cdebefc83cfe1147662b4c3ffbb4c6ae
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-11 20:58:43 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,11 @@
+package y7;
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama, alamat;
+        nama = "Asri";
+        alamat = "petoosang";
+        System.out.println("Nama\t : "+nama+"\nAlamat\t : "+alamat);
+    }
+    
+}
