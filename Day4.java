65c8b78eab0d77b72a7e57530a13010f3de04f19
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-12 23:10:07 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,17 @@
+package y12;
+
+public class Main {
+    public static void main(String[] args) {
+        String namaD, namaB, namabln;
+        byte tgl;
+        short thn;
+        System.out.println("=====Biodata Adolf Hitler=====\n");
+        namaD = "Adolf";
+        namaB = "Hitler";
+        tgl = 20;
+        namabln = "April";
+        thn = 1889;
+        System.out.println("Nama\t\t : "+namaD+" "+namaB);
+        System.out.println("Kelahiran\t : "+tgl+" "+namabln+" "+thn);
+    }
+}
