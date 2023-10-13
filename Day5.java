18314e1727371ce034afe4899784718fe8d0a129
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-14 02:41:25 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,12 @@
+package y16;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Asri";
+        char golD = 'O';    
+        System.out.print("Nama\t\t : "+nama+"\nGolongan darah\t : "+golD);
+       
+    }
+    
+}
