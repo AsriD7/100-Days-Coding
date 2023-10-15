ebaa22122f97cd432976d80e19448437325b8e41
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-15 23:51:05 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,14 @@
+package y16;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Assccl";
+        int hero = 122;
+        int skin = 298;
+        System.out.println("==========Akun Ml==========");
+        System.out.println("Nama akun\t : "+nama+"\nHero\t\t : "+hero+"\nSkin\t\t : "+skin);
+       
+    }
+    
+}
