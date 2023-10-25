a39299854eba5e23f09530959b4f8acd227d7674
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-25 17:40:23 +0800

    Operator perbandingan

---
+++ Day16.java
@@ -0,0 +1,10 @@
+package Day16;
+
+public class Main {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 7;
+        System.out.println(angka1 > angka2);
+        
+    }
+}
