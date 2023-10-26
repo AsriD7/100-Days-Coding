edd80fa2c04571b068794aab8dc0d1dbe2a57c2c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-26 18:35:05 +0800

    Belajar perulangan FOR yg di dalamnya ada inputan

---
+++ Day18.java
@@ -0,0 +1,16 @@
+package Day18;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan batasan angka : ");
+        int akhir = input.nextInt();
+        for (int i = 0; i < akhir; i++) {
+            System.out.print("Angka ke- ");
+            int angka = input.nextInt();
+            
+        }
+        
+        System.out.println("Perulangan selesai.");
+    }
+}
