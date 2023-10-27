3fe3918c8a750185bcd3ffb461120453e2b4fa15
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-27 18:13:08 +0800

    Membuat perulangan untuk mencetak angka dan nama sebanyak 10x

---
+++ Day19.java
@@ -0,0 +1,10 @@
+package Day19;
+public class Main {
+    public static void main(String[] args) {
+        //Soal membuat looping mencetak nama sbyk 10x
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i+" Asri");
+            
+        }
+    }
+}
