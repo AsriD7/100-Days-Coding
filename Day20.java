2f44a6eb21750260dcd57c984d41b26a62676ac1
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-29 13:46:32 +0800

    Konversi tipe data primitif ke String

---
+++ Day20.java
@@ -0,0 +1,12 @@
+package Day20;
+
+public class Main {
+    public static void main(String[] args){
+        int angka = 678;
+        String angkaString = Integer.toString(angka);
+        double angkaDouble = 3.147678;
+        String angkaDoubleString = Double.toString(angkaDouble);
+        System.out.println("Nilai\t:"+angkaString);
+        System.out.println("Nilai\t:"+angkaDoubleString);
+    }
+}
