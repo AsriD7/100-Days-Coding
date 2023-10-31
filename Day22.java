b47af6271eb1549f3972b22a79c3e495e9a9ea5b
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-31 15:42:13 +0800

    Program mencetak ganjil genap menggunakan operator ternary

---
+++ Day22.java
@@ -0,0 +1,16 @@
+package Day22;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int hasil, angka;
+        
+        System.out.print("Masukkan angka = ");
+        angka = input.nextInt();
+        hasil = angka % 2;
+        String k = hasil == 0?"Genap":"Ganjil";
+        System.out.println("Hasilnya = "+k);
+    }
+    
+}
