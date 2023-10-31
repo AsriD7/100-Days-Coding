19655faa8657f021621810d7077072ff75f51e21
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2023-10-31 23:02:15 +0800

    Belajar perulangan switch case

---
+++ Day23.java
@@ -0,0 +1,14 @@
+package Day23;
+public class Main {
+    public static void main(String[] args) {
+        int pil = 1;
+        switch(pil){
+            case 1:
+            System.out.println("laki-laki");
+            break;
+            case 2:
+            System.out.println("perempuan");
+            break;
+        }
+    }
+}
